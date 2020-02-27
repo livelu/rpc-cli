cbd59b3cb62812f03fb747f7b09e0edff2dcb60c
张璐 <zhanglu@xinchanedu.com>
2020-02-27 16:51:07 +0800

    viporder

---
+++ src/xiaoyingdeng/Viporder/ViporderClient.php
@@ -0,0 +1,32 @@
+<?php
+// GENERATED CODE -- DO NOT EDIT!
+
+namespace Viporder;
+
+/**
+ */
+class ViporderClient extends \Grpc\BaseStub {
+
+    /**
+     * @param string $hostname hostname
+     * @param array $opts channel options
+     * @param \Grpc\Channel $channel (optional) re-use channel object
+     */
+    public function __construct($hostname, $opts, $channel = null) {
+        parent::__construct($hostname, $opts, $channel);
+    }
+
+    /**
+     * @param \Viporder\Request $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function OrderList(\Viporder\Request $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/viporder.Viporder/OrderList',
+        $argument,
+        ['\Viporder\Responselist', 'decode'],
+        $metadata, $options);
+    }
+
+}
---
+++ src/xiaoyingdeng/Vipuser/VipuserClient.php
@@ -0,0 +1,45 @@
+<?php
+// GENERATED CODE -- DO NOT EDIT!
+
+namespace Vipuser;
+
+/**
+ */
+class VipuserClient extends \Grpc\BaseStub {
+
+    /**
+     * @param string $hostname hostname
+     * @param array $opts channel options
+     * @param \Grpc\Channel $channel (optional) re-use channel object
+     */
+    public function __construct($hostname, $opts, $channel = null) {
+        parent::__construct($hostname, $opts, $channel);
+    }
+
+    /**
+     * @param \Vipuser\Request $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function Login(\Vipuser\Request $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/vipuser.Vipuser/Login',
+        $argument,
+        ['\Vipuser\Response', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \Vipuser\Request $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function UserList(\Vipuser\Request $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/vipuser.Vipuser/UserList',
+        $argument,
+        ['\Vipuser\Responselist', 'decode'],
+        $metadata, $options);
+    }
+
+}
